fb9532baf008552a02914e7ecd5a3fbd9f69cd9c
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-01-27 09:08:43 +1300

    Move 'shared view path' env var up a level, to allow for more uses

--- config/application.rb
+++ config/application.rb
@@ -55,10 +55,10 @@ module Typo
 
     # Add the shared ROOL view components.
     #
-    shared_views_path = if ENV['SHARED_VIEWS_PATH'].blank?
+    shared_views_path = if ENV['SHARED_FILES_PATH'].blank?
       Rails.root.join('..', 'common', 'views')
     else
-      ENV['SHARED_VIEWS_PATH']
+      Rails.root.join(ENV['SHARED_FILES_PATH'], 'views')
     end
     config.paths['app/views'].unshift(shared_views_path)
 
